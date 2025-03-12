6bdbb11041943c58b90fcdba155c7afa0c5d3cf5
Safarov Safar <safarovsafar9888@gmail.com>
2025-03-12 15:39:07 +0500

    safar

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
     
 
     return (
-        <div className="dark:bg-black dark:text-white">
+        <div className="dark:bg-black dark:text-white h-screen overflow-hidden"> 
             <Header />
             <Outlet />
         </div>
